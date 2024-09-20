beb8bff71a7701668082402efc77cd155856a80a
Sahil Agrawal <agrawalsahil60@gmail.com>
2024-09-21 01:51:53 +0530

    Patch and delete methods added

--- src/main/java/com/example/TodoAPI/TodoController.java
+++ src/main/java/com/example/TodoAPI/TodoController.java
@@ -1,9 +1,8 @@
 package com.example.TodoAPI;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,8 +23,52 @@ public class TodoController {
     }
 
     @PostMapping("/todos")
+    @ResponseStatus(HttpStatus.CREATED)
     public Todo createTodo(@RequestBody Todo newTask) {
         todoList.add(newTask);
         return newTask;
     }
+
+    @GetMapping("/todos/{todoId}")
+    public ResponseEntity<?> getTaskbyId(@PathVariable long todoId) {
+        for(Todo todo : todoList) {
+            if(todo.getId() == todoId) {
+                return ResponseEntity.ok(todo);
+            }
+        }
+        //return ResponseEntity.notFound().build();
+        /*return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body((Todo) Collections.singletonMap("message", "Todo not found")); */
+
+        String jsonResponse = "Todo not found";
+
+        return new ResponseEntity<>(jsonResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping("/todos/{todoId}")
+    public void removeTask(@PathVariable long todoId) {
+        for(Todo todo : todoList){
+            if(todo.getId() == todoId) {
+                todoList.remove(todo);
+                //return ResponseEntity.ok(todo);
+            }
+        }
+    }
+
+    @PatchMapping("/todos/{todoId}")
+    public ResponseEntity<Todo> updateTodo(@PathVariable long todoId, @RequestBody Todo update ) {
+         for(Todo todo : todoList) {
+             if(todo.getId() == todoId) {
+                 todo.setTitle(update.getTitle());
+                 todo.setCompleted(update.isCompleted());
+                 return ResponseEntity.ok(todo);
+             }
+         }
+
+         return ResponseEntity.notFound().build();
+    }
+
+
 }
+
+
